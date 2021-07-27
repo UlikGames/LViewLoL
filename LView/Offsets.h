73d0e4a20056aa0974f2bbae611d5a1ec0b354d1
Ulvin <ulvin.oguzlu@gmail.com>
2021-07-27 12:40:42 +0400

    Update Offsets.h

--- LView/Offsets.h
+++ LView/Offsets.h
@@ -11,19 +11,30 @@ public:
 
 	static int ObjIndex;
 	static int ObjTeam;
+	static int ObjMissileName;
 	static int ObjNetworkID;
 	static int ObjPos;
+	static int ObjMissileSpellCast;
 	static int ObjVisibility;
 	static int ObjSpawnCount;
 	static int ObjHealth;
 	static int ObjMaxHealth;
+	static int ObjRecall;
 	static int ObjMana;
+	static int ObjMaxMana;
+	static int ObjRecallState;
+	static int ObjRecallStateSize;
+	static int ObjAbilityHaste;
+	static int ObjLethality;
 	static int ObjArmor;
+	static int ObjBonusArmor;
 	static int ObjMagicRes;
+	static int ObjBonusMagicRes;
 	static int ObjBaseAtk;
 	static int ObjBonusAtk;
 	static int ObjMoveSpeed;
 	static int ObjSpellBook;
+	static int ObjTransformation;
 	static int ObjName;
 	static int ObjLvl;
 	static int ObjExpiry;
@@ -33,10 +44,51 @@ public:
 	static int ObjAtkSpeedMulti;
 	static int ObjItemList;
 	static int ObjSrcIndex;
+	static int ObjAtkRange;
+	static int ObjTargetable;
+	static int ObjInvulnerable;
+	static int ObjIsMoving;
+	static int ObjDirection;
+	static int ObjExpierience;
+	static int ObjMagicPen;
+	static int ObjMagicPenMulti;
+	static int ObjAdditionalApMulti;
+	static int ObjManaRegen;
+	static int ObjHealthRegen;
+
+	static int ZoomClass;
+	static int MaxZoom;
+
+	static int Chat;
+	static int ChatIsOpen;
+
+	static int SpellBookActiveSpellCast;
+	static int SpellBookSpellSlots;
+
+	static int ObjBuffManager;
+	static int BuffManagerEntriesArray;
+	static int BuffEntryBuff;
+	static int BuffType;
+	static int BuffEntryBuffCount;
+	static int BuffEntryBuffCountAlt;
+	static int BuffEntryBuffCountAlt2;
+	static int BuffEntryBuffStartTime;
+	static int BuffEntryBuffEndTime;
+	static int BuffName;
+	static int BuffEntryBuffNodeStart;
+	static int BuffEntryBuffNodeCurrent;
 
 	static int ItemListItem;
 	static int ItemInfo;
 	static int ItemInfoId;
+
+	static int CurrentDashSpeed;
+	static int IsDashing;
+	static int DashPos;
+	static int IsMoving;
+	static int NavBegin;
+	static int NavEnd;
+	static int HudInstance;
 
 	static int ViewProjMatrices;
 	static int Renderer;
@@ -45,11 +97,15 @@ public:
 
 	static int SpellSlotLevel;
 	static int SpellSlotTime;
+	static int SpellSlotCharges;
+	static int SpellSlotTimeCharge;
 	static int SpellSlotDamage;
 	static int SpellSlotSpellInfo;
 	static int SpellInfoSpellData;
 	static int SpellDataSpellName;
 	static int SpellDataMissileName;
+	static int SpellSlotSmiteTimer;
+	static int SpellSlotSmiteCharges;
 
 	static int ObjectManager;
 	static int LocalPlayer;
@@ -65,9 +121,27 @@ public:
 	static int MissileDestIdx;
 	static int MissileStartPos;
 	static int MissileEndPos;
+	static int MissileCurrentPos;
+
+	static int SpellCastSpellInfo;
+	static int SpellCastStartTime;
+	static int SpellCastStartTimeAlt;
+	static int SpellCastCastTime;
+	static int SpellCastStart;
+	static int SpellCastEnd;
+	static int SpellCastSrcIdx;
+	static int SpellCastDestIdx;
 
 	static int MinimapObject;
 	static int MinimapObjectHud;
 	static int MinimapHudPos;
 	static int MinimapHudSize;
+
+	static int AiManagerStartPath;
+	static int AiManagerEndPath;
+	static int AiManagerTargetPosition;
+	static int AiManagerIsMoving;
+	static int AiManagerIsDashing;
+	static int AiManagerCurrentSegment;
+	static int AiManagerDashSpeed;
 };
